520ae72ebf915386f28c315e39ddeafabe7accac
Mohri Samuel Muakpo <sammuaks@gmail.com>
2026-01-20 03:16:36 +0000

    Add Nativewind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./app/**/*. {js, jsx, ts, tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
