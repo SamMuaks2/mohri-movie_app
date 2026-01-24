ca4f6b7bcc2c5826107569e14a02b901aea5fcf5
Mohri Samuel Muakpo <sammuaks@gmail.com>
2026-01-24 13:39:16 +0000

    Update props

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,14 +2,14 @@ import { icons } from '@/constants/icons'
 import { images } from '@/constants/images'
 import { Tabs } from 'expo-router'
 import React from 'react'
-import { Image, ImageBackground, Text } from 'react-native'
+import { Image, ImageBackground, Text, View } from 'react-native'
 
 const TabIcon = ({focused, icon, title}: any) => {
     if(focused) {
         return (
             <ImageBackground
                 source={images.highlight}
-                className='flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden'
+                className='flex flex-row w-full flex-1 min-w-[112px] min-h-16 mb-0 mt-4 justify-center items-center rounded-full overflow-hidden'
             >
                 <Image
                 source={icon}
@@ -24,11 +24,38 @@ const TabIcon = ({focused, icon, title}: any) => {
         )
     }
 
+    return (
+        <View className='size-full justify-center items-center mt-4 rounded-full'>
+            <Image source={icon} tintColor="#A8B5DB" className='size-5' />
+        </View>
+    )
+
 }
 
 const _layout = () => {
   return (
-    <Tabs>
+    <Tabs
+        screenOptions={{
+            tabBarShowLabel: false,
+            tabBarItemStyle: {
+                width: '100%',
+                height: '100%',
+                justifyContent: 'center',
+                alignItems: 'center'
+            },
+            tabBarStyle: {
+                backgroundColor: '#0f0D23',
+                borderRadius: 50,
+                marginHorizontal: 20,
+                marginBottom: 36,
+                height: 52,
+                position: 'absolute',
+                overflow: 'hidden',
+                borderWidth: 1,
+                borderColor: '#0f0D23',
+            }
+        }}
+    >
         <Tabs.Screen
             name="index"
             options={{
